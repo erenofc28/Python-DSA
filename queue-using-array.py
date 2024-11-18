ac394ec8c90859d7aa2e99420db7936142d7347f
A.Dhinesh Kumar <161221701+erenofc28@users.noreply.github.com>
2024-11-18 16:22:21 +0530

    Update queue-using-array.py

--- queue-using-array.py
+++ queue-using-array.py
@@ -1,13 +1,24 @@
 class queue:
     def __init__(self):
         self.queue = []
-
+        self.front = -1
+        self.rear = -1
     def enqueue(self,val):
+        if(len(self.queue)==0):
+            self.front += 1
+            self.rear += 1
+            self.queue.append(val)
+            return
         self.queue.append(val)
-
+        self.rear += 1
+        
     def dequeue(self):
+        if(len(self.queue)==0):
+            print("queue is empty")
+            return
         print(self.queue.pop(0),"removed from queue")
-    
+        self.rear -= 1
+        
     def peek(self):
         if(len(self.queue)==0):
             print("queue is empty")
@@ -25,6 +36,7 @@ q.enqueue(3)
 q.display()
 print("value added into queue")
 q.enqueue(4)
+
 q.display()
 q.dequeue()
 q.display()
